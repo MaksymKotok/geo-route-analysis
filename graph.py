c982b18b09a49f8961257dd3e8f0a225c7748bdd
Maksym Kotok <max.kotok21@gmail.com>
2023-12-03 16:49:54 +0200

    Added multi-route plotting

--- graph.py
+++ graph.py
@@ -5,27 +5,52 @@ import osmnx as ox
 import networkx as nx
 import webbrowser
 import os
+from random import randint
 
 ox.settings.use_cache=True
 ox.settings.log_console=True
 
+COLORS = [
+    'red',
+    'blue',
+    'gray',
+    'darkred',
+    # 'lightred',
+    'orange',
+    # 'beige',
+    'green',
+    'darkgreen',
+    # 'lightgreen',
+    'darkblue',
+    'lightblue',
+    'purple',
+    'darkpurple',
+    'pink',
+    # 'cadetblue',
+    # 'lightgray',
+    # 'black'
+]
 
 def render_heatmap(df: pd.DataFrame) -> str:
     center = (49.83, 24.02)
 
     _map = folium.Map(
-        location=center, 
+        location=center,
+        tiles='cartodbpositron',
         zoom_start=13, 
         control_scale=True
     )
     
     data = df[["LATITUDE", "LONGITUDE"]].values.tolist()
-    _heatmap = HeatMap(
+    _ = HeatMap(
         data,
         min_opacity=0.05,
         max_opacity=0.9,
         radius=25
     ).add_to(_map)
+
+    if not os.path.exists('maps/'):
+        os.makedirs('maps/')
 
     path = 'maps/heatmap.html'
     _map.save(path)
@@ -40,42 +65,47 @@ def render_route(df: pd.DataFrame) -> str:
 
     _points = [(row["LATITUDE"], row["LONGITUDE"]) for _, row in df.iterrows()]
 
+    # FILTERING POINTS THAT ARE LOCATED IN LVIV
     points = []
     dist = lambda x, y: ((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2) ** (1/2)
-    center = (49.83, 24.02)
+    center = (49.83, 24.02) # CENTER OF LVIV
     radius = 0.06
     for p in _points:
         if dist(center, p) < radius:
             points.append(p)
 
     _map = folium.Map(
-        location=center, 
+        location=center,
+        tiles='cartodbpositron',
         zoom_start=13, 
         control_scale=True
     )
 
-    G_walk = ox.graph_from_place("Lviv, Ukraine", network_type='drive', simplify=True, retain_all=False)
-    orig_node = ox.nearest_nodes(G_walk, points[0][1], points[0][0])
-    dest_node = ox.nearest_nodes(G_walk, points[-1][1], points[-1][0])
+    folium.Marker(
+        location=points[0],
+        popup=folium.Popup(get_frame(0, points[0][0], points[0][1]))
+    ).add_to(_map)
 
-    route = nx.shortest_path(G_walk, orig_node, dest_node, weight='length')
-    _map = ox.plot_route_folium(G_walk, route)
+    for i in range(len(points) - 1):
+        G_walk = ox.graph_from_place("Lviv, Ukraine", network_type='drive', simplify=True, retain_all=False)
+        orig_node = ox.nearest_nodes(G_walk, points[i][1], points[i][0])
+        dest_node = ox.nearest_nodes(G_walk, points[i + 1][1], points[i + 1][0])
 
-    # for i in range(len(points)):
-    folium.Marker(location=points[0], popup=folium.Popup(get_frame(0, points[0][0], points[0][1]))).add_to(_map)
-    folium.Marker(location=points[-1], popup=folium.Popup(get_frame(1, points[-1][0], points[-1][1]))).add_to(_map)
+        route = nx.shortest_path(G_walk, orig_node, dest_node, weight='length', method='dijkstra')
+
+        color = COLORS[randint(0, len(COLORS) - 1)]
+        _map = ox.plot_route_folium(G_walk, route, route_map=_map, color=color)
+
+        folium.Marker(
+            location=points[i + 1],
+            popup=folium.Popup(get_frame(i + 1, points[i + 1][0], points[i + 1][1])),
+            icon=folium.Icon(color=color)
+        ).add_to(_map)
+
+    if not os.path.exists('maps/'):
+        os.makedirs('maps/')
 
     path = 'maps/route.html'
     _map.save(path)
     webbrowser.open(f'file://{os.path.abspath(path)}')
     return path
-
-
-# if __name__ == '__main__':
-#     df = pd.read_csv('csv/266.csv')
-#     df.rename({
-#         1: "LATITUDE",
-#         2: "LONGITUDE"
-#     })
-#     # render_heatmap(df)
-#     render_route(df)
